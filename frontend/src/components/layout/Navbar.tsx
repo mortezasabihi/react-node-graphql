e9da5f47474f286b7f35decc250b76e7be89d318
Morteza Sabihi <mortezasabihi87@gmail.com>
2022-03-21 12:27:21 +0330

    navbar component added

---
+++ frontend/src/components/layout/Navbar.tsx
@@ -0,0 +1,32 @@
+import { Flex, Button, Heading } from "@chakra-ui/react";
+import Link from "next/link";
+
+const Navbar = () => (
+  <Flex
+    as={"nav"}
+    alignItems={"center"}
+    justifyContent={"space-between"}
+    p={4}
+    bg={"white"}
+    borderBottom={"1px solid #f0f0f0"}
+    marginTop={4}
+    marginBottom={4}
+  >
+    <Flex alignItems={"center"}>
+      <Heading as={"h1"} size={"md"}>
+        <Link href={"/"}>
+          <a>Users List</a>
+        </Link>
+      </Heading>
+    </Flex>
+
+    <Flex alignItems={"center"}>
+      <Link href={"/add"} passHref>
+        <Button colorScheme={"blue"} as={"a"} size={"sm"}>
+          Add user
+        </Button>
+      </Link>
+    </Flex>
+  </Flex>
+);
+export default Navbar;
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Container } from "@chakra-ui/react";
 import theme from "src/theme";
 import "@fontsource/raleway/400.css";
 import "@fontsource/open-sans/700.css";
+import Navbar from "src/components/layout/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Container maxW={"container.lg"}>
+        <Navbar />
+        <Component {...pageProps} />
+      </Container>
     </ChakraProvider>
   );
 }
